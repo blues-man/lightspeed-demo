85c6f52057824e501a0c45f395199606abb43d30
Natale Vinto <nvinto@redhat.com>
2025-05-13 00:52:59 +0200

    Add exception if missing env MODE

--- src/main/java/com/redhat/GreetingResource.java
+++ src/main/java/com/redhat/GreetingResource.java
@@ -4,6 +4,7 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.NotFoundException;
 
 @Path("/hello")
 public class GreetingResource {
@@ -11,6 +12,10 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkus REST";
+        String env = System.getenv("MODE");
+        if (env == null) {
+            throw new RuntimeException("Environment variable MODE is not set");
+        }
+        return "Hello from ACME REST Quarkus with mode " + env;
     }
 }
